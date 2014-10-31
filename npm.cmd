cd544888b5a5c591c5850949bc935121b2972d68
Stéphane ZUCATTI <stephane.zucatti@me.com>
2014-10-31 11:42:02 +0100

    Update npm.cmd

--- npm.cmd
+++ npm.cmd
@@ -1,2 +1,3 @@
-  node "%~dp0\npm-cli.js" config set cache "%~dp0\tmp" --global
-  node "%~dp0\npm-cli.js" %*
+  @ECHO OFF
+  @node "%~dp0\npm-cli.js" config set cache "%~dp0\tmp" --global
+  @node "%~dp0\npm-cli.js" %*
